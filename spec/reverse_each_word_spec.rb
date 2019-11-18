9ddca2e01e43828617ee6a3adb398304b5e68344
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-11-18 21:00:50 +0000

    Automatically backed up by Learn

--- spec/reverse_each_word_spec.rb
+++ spec/reverse_each_word_spec.rb
@@ -2,5 +2,6 @@ favorites = ("Zelda", "God of War", "NBA2K")
 video_games(favorites)
   favorites.collect do |favorites|
   puts "The best video games are #{favorites}"
+end
 
-    
+reverse_each_word(favorites)
